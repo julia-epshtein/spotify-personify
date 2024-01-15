5dbe4175cc9c395683f8d817fc77747676fc650b
Julia Epshtein <jepshtein03@gmail.com>
2024-01-15 01:15:06 -0500

    wrote some skeleton code for topsongs. Need to retrieve access token

--- spotifystats/app/stats/topsongs.tsx
+++ spotifystats/app/stats/topsongs.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+const API_BASE_URL = 'https://api.spotify.com/v1/'
 
-function Topsongs() {
+function Topsongs({accessToken=''}) {
+  const [topSongs, setTopSongs] = useState([]);
+
+  // Function to retrieve top songs from Spotify API
+  const fetchTopSongs = async () => {
+    try {
+      // Fetch top songs from Spotify API
+      const response = await fetch("https://api.spotify.com/v1/me/top/tracks", {
+        headers: {
+          Authorization: 'Bearer ' + accessToken
+        }
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setTopSongs(data.items); // Assuming the API response has an 'items' property
+      } else {
+        console.error("Error fetching top songs:", response.statusText);
+      }
+    } catch (error) {
+      console.error("An error occurred while fetching top songs:", error);
+    }
+  };
+
   return (
-    <div className='w-screen h-screen flex flex-col bg-rose-500/80'>
-      Top songs
+    <div className="w-screen h-screen flex flex-col bg-white">
+      <p className="text-6xl text-black underline underline-offset-8 m-12">
+        Your Top Songs
+      </p>
     </div>
-  )
+  );
 }
 
-export default Topsongs
+//retrieve top songs from spotify api
+
+export default Topsongs;
